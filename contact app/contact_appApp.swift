46de4b1b5d94c9845e339c290fe7b41bb25ec8a1
LongLong0606 <goldsky284@gmail.com>
2023-07-26 10:30:47 +0700

    Initial Commit

---
+++ contact app/contact_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  contact_appApp.swift
+//  contact app
+//
+//  Created by Phạm Long on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct contact_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
